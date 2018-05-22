69dd2ca9de1ea25631a419420de8b0aab21273cc
Jack Huggart <Jack Huggart>
2018-05-22 10:46:49 +1000

    HealthKit implementation in root view controller

--- HealthPlus/ViewController.swift
+++ HealthPlus/ViewController.swift
@@ -7,19 +7,34 @@
 //
 
 import UIKit
+import HealthKit
 
 class ViewController: UIViewController {
+    var healthStore : HKHealthStore?
+    
+    func requestPerms() {
+        let allDataTypes = Set([HKObjectType.quantityType(forIdentifier: .distanceWalkingRunning)!,
+                                HKObjectType.quantityType(forIdentifier: .heartRate)!])
+        
+        healthStore!.requestAuthorization(toShare: allDataTypes, read: allDataTypes) { (success, error) in
+            if !success {
+                print("Failed to access")
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if HKHealthStore.isHealthDataAvailable() {
+            healthStore = HKHealthStore()
+            requestPerms()
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
