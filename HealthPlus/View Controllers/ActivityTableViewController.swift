07c3f75aa52799e331ae92d57d7fc76e944016c6
Proma <11908415@student.uts.edu.au>
2018-05-30 03:52:53 +1000

    Added initial demo of SceneKit when selecting body part for exercise. TODO: Convert the ExerciseViewController to a TableViewController

--- HealthPlus/View Controllers/ActivityTableViewController.swift
+++ HealthPlus/View Controllers/ActivityTableViewController.swift
@@ -9,12 +9,28 @@
 import Foundation
 import HealthKit
 import UIKit
+import MapKit
 
-class ActivityTableViewController : UITableViewController {
+
+class ActivityTableViewController : UITableViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     var healthStore : HKHealthStore?
+    let locationManager = CLLocationManager()
+    
     @IBOutlet weak var bpmTxt: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
+        mapView.delegate = self
+        mapView.showsPointsOfInterest = true
+        mapView.showsUserLocation = true
+        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
         let hrType = HKObjectType.quantityType(forIdentifier: .heartRate)
         
         let hrQuery = HKSampleQuery(sampleType: hrType!,
@@ -31,5 +47,23 @@ class ActivityTableViewController : UITableViewController {
         }
         
         healthStore?.execute(hrQuery)
+    }
+    //Override the Update location function, so that I can act whenever iOS updates the map with my new location
+    func locationManager(_ _manager : CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last!
+        //Set to the whole area of the map shows 500 by 500
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, 500, 500)
+        mapView.setRegion(coordinateRegion, animated: true)
+        //Set my current location using a 2d point so I can being drawing with it.
+        let currLocation = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        
+        locationManager.stopUpdatingLocation()
+    }
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.strokeColor = UIColor.red
+        renderer.lineWidth = 4.0
+        
+        return renderer
     }
 }
